bbcb0aca285d6d2fe9d72c8b5b056f55c8f8ba77
Nihal Coşkun <77547456+nihalcoskun@users.noreply.github.com>
2022-11-02 20:36:33 +0300

    Update main.java

--- main.java
+++ main.java
@@ -31,113 +31,109 @@ public class Project {
 		
 		try 
 		 {
-			     File sourceFile = new File (filePath);
-				 String fileName = sourceFile.getName();
+	          File sourceFile = new File (filePath);
+		  String fileName = sourceFile.getName();
 				 
-				 Scanner scanner = new Scanner(sourceFile);
-				 ArrayList<String> notFounds = new ArrayList<String>();
-				 ArrayList<String> currencies = new ArrayList<String>();
-				 String merchant = null;
-				 String currency = null;
-				 while(scanner.hasNextLine())
-					{	
-						String line = scanner.nextLine();
-						String [] split = line.split(",");
-						
-						
-						if(fileName.startsWith("Z5"))
-					 	{
+		  Scanner scanner = new Scanner(sourceFile);
+		  ArrayList<String> notFounds = new ArrayList<String>();
+		  ArrayList<String> currencies = new ArrayList<String>();
+		  String merchant = null;
+	       	  String currency = null;
+ 		  while(scanner.hasNextLine())	
+		  {
+		     String line = scanner.nextLine();
+		     String [] split = line.split(",");
+									
+		     if(fileName.startsWith("Z5"))
+		     {
 						 	
-							if(split.length >= 18) 
-							   {			
-								   if(split[18].isEmpty())
-								   {
-										merchant = null;
-								   }
-								   else 
-								   {
-										merchant = split[18].substring(4);
-										if(merchant.startsWith("0"))
-										{
-											merchant = merchant.substring(1);
+			if(split.length >= 18) 
+			{			
+			   if(split[18].isEmpty())
+			   {
+				merchant = null;
+			   }
+			   else 
+			   {
+				   
+				merchant = split[18].substring(4);
+				if(merchant.startsWith("0"))
+				{
+					merchant = merchant.substring(1);
 										}
-										
-									if(split[4].isEmpty())
-									{
-										currency = null;
-									}
-									else
-										currency = split[4];
-								   }
-							   }
+					if(split[4].isEmpty())
+			         	{
+						currency = null;
+					}
+					else
+		            			currency = split[4];
+				  }
+			   }
 							
-					 	}
+		     }
 						
-					     if(fileName.startsWith("Z1"))
-					 	{
+		     if(fileName.startsWith("Z1"))
+		     {
 						 
-					    	 if(split[0].contentEquals("01") && split.length >= 2) 
-							   {
-								   merchant = split[2];
+			 if(split[0].contentEquals("01") && split.length >= 2) 
+			 {
+				merchant = split[2];
 								   
-								   if(split.length >= 13)
-									 currency = split[13];
+				if(split.length >= 13)
+				currency = split[13];
 							   							   
-							   }					    	 					    	 
+			  }					    	 					    	 
 					    	 					    	 
-					 	}
-					     
-					     if(fileName.startsWith("AX"))
-						 	{
-							 
-						    	 if( split[0].contentEquals("\"TRANSACTN\"") && split.length >= 6) 
-								   {			
-									   if(split[6].isEmpty())
-									   {
-											merchant = null;
+		      }
+		     
+		      if(fileName.startsWith("AX"))
+		      {
+						 
+			  if( split[0].contentEquals("\"TRANSACTN\"") && split.length >= 6) 
+			   {			
+				   if(split[6].isEmpty())
+				   {
+					merchant = null;
+				   }
+			   else
+	         	   {
+				merchant = split[6].substring(1,11);
+				if(split[10].isEmpty())
+				{
+	            			currency = null;
+				}
+				else
+					currency = split[10].substring(1,4);
 									   }
-									   else
-									   {
-											merchant = split[6].substring(1,11);
-											if(split[10].isEmpty())
-											{
-												currency = null;
-											}
-											else
-												currency = split[10].substring(1,4);
-									   }
-								   }
+			   }
 						    	 
 						    	 
-						 	}
-					     
-
-								
+	               }
+					     						
 				 			 				     	
 				 
 					  
-
-					    if(merchant != null )
-					    { 	
-					    	ResultSet set = null;
-					    	if(fileName.startsWith("Z5"))
-					    	{
-					    	set = statement.executeQuery("SELECT * FROM SAP_MERCHANT_MASTER WHERE MERCHANT_ID = '" + merchant + "' AND SETTLEMENT_CURRENCY = '" + currency + "'" );
-					    	}
-					    	if(fileName.startsWith("Z1") || fileName.startsWith("AX") )
-					    	{
-					    	 set = statement.executeQuery("SELECT * FROM SAP_MERCHANT_MASTER WHERE MERCHANT_ID = '" + merchant + "' AND CURRENCY = '" + currency + "'" );
-					    	}
+		      if(merchant != null )
+		      { 	
+ 		    	 ResultSet set = null;
+			 if(fileName.startsWith("Z5"))
+		    	 {
+			   set = statement.executeQuery("SELECT * FROM SAP_MERCHANT_MASTER WHERE MERCHANT_ID = '" + merchant + "' AND SETTLEMENT_CURRENCY = '" + currency + "'" );
+		    	 }
+			 if(fileName.startsWith("Z1") || fileName.startsWith("AX") )
+		    	 {
+	               	   set = statement.executeQuery("SELECT * FROM SAP_MERCHANT_MASTER WHERE MERCHANT_ID = '" + merchant + "' AND CURRENCY = '" + currency + "'" );
+		    	 }
 					    	
-					    if(!set.next())
-			            {	
-			            	if(!notFounds.contains(merchant)) 
-					         {
-					            	notFounds.add(merchant);
-					            	currencies.add(currency);
-					          }
-					     }           
-					    }
+	               if(!set.next())
+	               {	
+	            	if(!notFounds.contains(merchant)) 
+		          {
+		            	notFounds.add(merchant);
+		            	currencies.add(currency);
+			 }
+		        }           
+		     }
 					       
 
 				       }
